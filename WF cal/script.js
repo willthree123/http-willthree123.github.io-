4855c8f36bcb0fc8d17cf4b01f5d9044bd2209ec
willthree123 <50272419+willthree123@users.noreply.github.com>
2020-07-03 01:08:01 +0800

    Update

--- WF cal/script.js
+++ WF cal/script.js
@@ -2,8 +2,9 @@ function myFunction() {
 
 var g=+document.getElementById("weight").value;
 var l=+document.getElementById("legth").value;
+
 var h=+document.getElementById("hour").value;
-var h=+document.getElementById("min").value;
-t=h*60+m
-document.getElementById("price").innerHTML = '$'+Math.round(5 +  (0.4*(g+l)) + (0.1*t));
+var m=+document.getElementById("min").value;
+
+document.getElementById("price").innerHTML = '$'+Math.round(5 +  (0.4*(g+l)) + (0.1*(h*60+m)));
 }

6d33ceaf272fe74b12a23b1ab7d289f668c23089
willthree123 <willthree123@gmail.com>
2022-09-04 21:17:16 +0800

    scroll

--- flip.js
+++ flip.js
@@ -1,8 +1,10 @@
 const card = document.querySelector('.card');
 const p = document.querySelector('.pulseDiv');
+const b = document.querySelector('.body');
 
 card.addEventListener('click', function() {
     card.classList.toggle('flipped');
     p.classList.toggle('unone');
+    b.classList.toggle('scroll');
     // window.alert("sometext");
 });

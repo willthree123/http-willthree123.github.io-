d2bf6ae12abe0660ad54ce58392c326b0098d369
willthree123 <willthree123@gmail.com>
2022-09-05 19:56:59 +0800

    testing

--- flip.js
+++ flip.js
@@ -1,10 +1,36 @@
 const card = document.querySelector('.card');
+const front = document.querySelector('.front');
+const back = document.querySelector('.backimg');
 const p = document.querySelector('.pulseDiv');
 const b = document.querySelector('.body');
+const l = document.querySelector('.linkSection');
 
-card.addEventListener('click', function() {
+// card.addEventListener('click', function() {
+//     card.classList.toggle('flipped');
+//     p.classList.toggle('unone');
+//     b.classList.toggle('scroll');
+//     l.classList.toggle("show");
+//     // window.alert("sometext");
+// });
+
+front.addEventListener('click', function() {
     card.classList.toggle('flipped');
     p.classList.toggle('unone');
     b.classList.toggle('scroll');
+    l.classList.toggle("show");
     // window.alert("sometext");
+});
+back.addEventListener('click', function() {
+    card.classList.toggle('flipped');
+    p.classList.toggle('unone');
+    b.classList.toggle('scroll');
+    l.classList.toggle("show");
+    // window.alert("sometext");
+});
+
+const pay = document.querySelector('.pay');
+const payDiv = document.querySelector('.payDiv');
+pay.addEventListener('click', function() {
+    payDiv.classList.toggle('expanded');
+    pay.classList.toggle("less")
 });
